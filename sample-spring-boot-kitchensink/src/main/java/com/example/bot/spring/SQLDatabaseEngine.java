77761d1a556bb81df7900a11539656b32578f027
Uday Sharma <us2596@gmail.com>
2017-10-21 01:02:07 +0800

    lab redone

---
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLDatabaseEngine.java
@@ -0,0 +1,58 @@
+package com.example.bot.spring;
+
+import lombok.extern.slf4j.Slf4j;
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+import java.sql.*;
+import java.net.URISyntaxException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+
+@Slf4j
+public class SQLDatabaseEngine extends DatabaseEngine {
+	@Override
+	String search(String text) throws Exception {
+		//Write your code here
+		String result = null;
+		try {
+			Connection con = getConnection();
+			PreparedStatement smt = con.prepareStatement("SELECT response FROM msg where keyword like concat('%', ? ,'%')");
+			smt.setString(1,text);
+			ResultSet rs = smt.executeQuery();
+			while(rs.next())
+			{
+				result = rs.getString("response");
+			}
+			rs.close();
+			smt.close();
+			con.close();
+		}catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		if(result!=null)
+			return result;
+		throw new Exception("NOT FOUND");
+		
+	}
+	
+	
+	private Connection getConnection() throws URISyntaxException, SQLException {
+		Connection connection;
+		URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+		String username = dbUri.getUserInfo().split(":")[0];
+		String password = dbUri.getUserInfo().split(":")[1];
+		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() +  "?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
+
+		log.info("Username: {} Password: {}", username, password);
+		log.info ("dbUrl: {}", dbUrl);
+		
+		connection = DriverManager.getConnection(dbUrl, username, password);
+
+		return connection;
+	}
+
+}
