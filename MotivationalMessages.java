7f8a230928e7f9a4f9d78fa5e0263ca1edd08ca2
vinmis <vinamra.misra@gmail.com>
2017-11-02 16:04:18 +0800

    Recommendation
    System

--- MotivationalMessages.java
+++ MotivationalMessages.java
@@ -1,4 +1,10 @@
 
 public class MotivationalMessages {
+	string Message1 = "Good progress! One more step towards a healthier lifestyle";
+	string Message2 = "Add oil!";
+	string Message3 = "Strive for progress, not perfection";
+	string Message3 = "The struggle you're in today is developing the strength you need for tomorrow";
+	
+	
 
 }
---
+++ Reccomendation.java
@@ -0,0 +1,28 @@
+public class Recommendation{
+	private String menu;
+	private float cal;
+	
+	public String getMenu() {return menu; }
+	public float getCal() {return cal; }
+
+	public Recommendation[] sortedMenu(Recommendation r [int n] ){
+		
+		for(int i=1; i<n;i++)
+		{
+			int diff=abs(r[i].cal-x);
+			// Insert arr[i] at correct place
+	        int j = i - 1;
+	        if (abs(r[i].cal - x) > diff) {
+	            recommendation temp = r[i];
+	            while (abs(r[j].cal - x) > diff && j >= 0) {
+	                r[j + 1] = r[j];
+	                j--;
+	            }
+	            r[j + 1] = temp;
+	        }
+	    }
+	
+	} 
+
+} 
+
