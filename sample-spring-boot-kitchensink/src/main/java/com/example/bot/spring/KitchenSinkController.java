41e30a663fd7200fcb64a04f16d9d801af945e1e
Akash <akash@Akashs-MacBook-Pro.local>
2017-11-14 00:08:03 +0800

    m

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -211,7 +211,7 @@ public class KitchenSinkController {
 		reply(replyToken, new StickerMessage(content.getPackageId(), content.getStickerId()));
 	}
 
-	private void handleTextContent(String replyToken, Event event, TextMessageContent content)
+	private void handleTextContent(String replyToken, Event event, TextMessageContent content) 
             throws Exception {
         String text = content.getText();
         String[] arr = text.split(":");
