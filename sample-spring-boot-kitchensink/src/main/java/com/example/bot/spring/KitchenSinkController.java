d0b065e869205e9a71316ebce78b33b7eef983d3
nimishagoyal <goyalnimisha2010@gmail.com>
2017-11-18 21:40:49 +0800

    testing

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -172,7 +172,7 @@ public class KitchenSinkController {
 		String replyToken = event.getReplyToken();
 		System.out.println(event.getSource().getUserId());
 		String cal = (event.getPostbackContent().getData()).substring(event.getPostbackContent().getData().lastIndexOf(" ")+1);
-		this.replyText(replyToken, (event.getPostbackContent().getData()).substring(0, (event.getPostbackContent().getData()).lastIndexOf(" ")));
+		this.replyText(replyToken, (event.getPostbackContent().getData()).substring(0, (event.getPostbackContent().getData()).lastIndexOf(" "))+" lolol "+cal);
 	}
 
 	@EventMapping
