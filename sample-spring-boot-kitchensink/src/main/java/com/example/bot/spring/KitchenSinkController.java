0a1c77e3a5ccd473832250b8bc4eab9eb81f438b
Uday Sharma <us2596@gmail.com>
2017-11-18 19:13:38 +0800

    fsd

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -342,7 +342,7 @@ public class KitchenSinkController {
         	Dish[] final_dishes = recomDB.findCaloricContent(dishes2);
         	User curr_user = database.getUserRecord(userId);
         	String[] a = curr_user.getRestrictions().split(",");
-        	System.out.println(a);
+        	System.out.println(a[0] +"\t" +a[1]);
         	Recommendation recommend = new Recommendation(curr_user, final_dishes);
 //        	log.info("inputted dishes: "+recommend.getInputDishes());
         	Dish[] recommended_dishes;
