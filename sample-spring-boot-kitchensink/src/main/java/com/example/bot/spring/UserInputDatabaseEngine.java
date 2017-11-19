2186e81330f1d8106437f5bc84c5af03762422a7
nimishagoyal <goyalnimisha2010@gmail.com>
2017-11-19 16:30:28 +0800

    test

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/UserInputDatabaseEngine.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/UserInputDatabaseEngine.java
@@ -190,8 +190,8 @@ public class UserInputDatabaseEngine extends DatabaseEngine {
 
 			PreparedStatement smt2 = con.prepareStatement("UPDATE userdatatable SET calperday=?, dates=? WHERE user_id=?");
 //			smt2.setString(1,totalCalList);
-			smt2.setString(1,"50");
-			smt2.setString(2,"12-12-2012");
+			smt2.setString(1,totalCalList);
+			smt2.setString(2,totalDates);
 			smt2.setString(3,UserId);
 			System.out.println("\n\n\n\n"+smt2+"\n\n\n\n");
 			ResultSet rs2 = smt2.executeQuery();
@@ -200,7 +200,6 @@ public class UserInputDatabaseEngine extends DatabaseEngine {
 			smt2.close();
 			smt1.close();
 			con.close();
-			return totalCalList;
 
 		}
 		catch (Exception e) {
